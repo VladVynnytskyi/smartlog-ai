8d3f93ef3f8f043c07c65ef7a1f3c2382adf4169
VladVynnytskyi <vynnytskyivladyslav@gmail.com>
2025-07-10 23:16:06 +0200

    Style: clean up formatting in log_parser.py

--- log_parser.py
+++ log_parser.py
@@ -4,28 +4,30 @@ warn = []
 def readlog():
     with open("sample.log", "r") as file:
         lines = file.readlines()
-        for line in lines: #!!!!
+        for line in lines:
             if "ERROR" in line:
                 errors.append(line)
 
-
 def count_warnings():
-    with open("sample.log") as file:
+    with open("sample.log", "r") as file:
         lines = file.readlines()
         for line in lines:
             if "WARNING" in line:
                 warn.append(line)
 
+if __name__ == "__main__":
+    ask = int(input("Choose options, print 1 or 2 or 3\n"
+                    "1) Errors\n"
+                    "2) Warnings\n"
+                    "3) ...\n"))
 
-if __name__=="__main__": 
-    ask = int(input("Choose options, print 1 or 2 or 3\n 1)errors\n2)Warnings\n3)...\n "))
     if ask == 1:
         readlog()
         print(errors)
     elif ask == 2:
         count_warnings()
         print(warn)
-    elif ask ==3:
+    elif ask == 3:
         print()
     else:
         print("Choose correct option")
