3e30dcde5552e014cf7c8c1397ad6c7f101b0ea0
VladVynnytskyi <vynnytskyivladyslav@gmail.com>
2025-07-10 23:14:38 +0200

    Clean up code comments

--- log_parser.py
+++ log_parser.py
@@ -1,8 +1,6 @@
 errors = []
 warn = []
 
-# ask = int(input("Choose options, print 1 or 2 or 3 1)errors/2).../3).../ "))
-
 def readlog():
     with open("sample.log", "r") as file:
         lines = file.readlines()
@@ -19,7 +17,7 @@ def count_warnings():
                 warn.append(line)
 
 
-if __name__=="__main__": #Якщо ти запустиш log_parser.py напряму — код у if __name__ == "__main__" виконається, Якщо ж ти імпортуєш його через import log_parser у main.py — код НЕ виконається.
+if __name__=="__main__": 
     ask = int(input("Choose options, print 1 or 2 or 3\n 1)errors\n2)Warnings\n3)...\n "))
     if ask == 1:
         readlog()
