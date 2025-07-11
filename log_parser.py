504f08e4d142432d4ee1902494c0562f8ecf4500
VladVynnytskyi <vynnytskyivladyslav@gmail.com>
2025-07-11 21:57:23 +0200

    Add IP address parsing with regex

--- log_parser.py
+++ log_parser.py
@@ -1,7 +1,9 @@
+import re
+
 errors = []
 warn = []
 
-def readlog():
+def read_log():
     with open("sample.log", "r") as file:
         lines = file.readlines()
         for line in lines:
@@ -15,19 +17,30 @@ def count_warnings():
             if "WARNING" in line:
                 warn.append(line)
 
+def find_ips():
+    with open("sample.log", "r") as file:
+        lines = file.readlines()
+        ips = set()
+        for line in lines:
+            found_ips = re.findall(r'\b(?:\d{1,3}\.){3}\d{1,3}\b', line)
+            for ip in found_ips:
+                ips.add(ip)
+        print(ips)
+
+
 if __name__ == "__main__":
     ask = int(input("Choose options, print 1 or 2 or 3\n"
                     "1) Errors\n"
                     "2) Warnings\n"
-                    "3) ...\n"))
+                    "3) Ips \n"))
 
     if ask == 1:
-        readlog()
+        read_log()
         print(errors)
     elif ask == 2:
         count_warnings()
         print(warn)
     elif ask == 3:
-        print()
+        find_ips()
     else:
         print("Choose correct option")
