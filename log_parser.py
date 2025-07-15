e6e8217104bf2de8197949da36b8dff2775e8177
VladVynnytskyi <vynnytskyivladyslav@gmail.com>
2025-07-15 23:43:23 +0200

    fix all bugs with ips.txt

--- log_parser.py
+++ log_parser.py
@@ -2,43 +2,43 @@ import re
 
 errors = []
 warn = []
-ips = []
 
 def read_log():
     with open("sample.log", "r") as file:
         lines = file.readlines()
-        for line in lines:
-            if "ERROR" in line:
-                errors.append(line)
+    for line in lines:
+        if "ERROR" in line:
+            errors.append(line)
 
 def count_warnings():
     with open("sample.log", "r") as file:
         lines = file.readlines()
-        for line in lines:
-            if "WARNING" in line:
-                warn.append(line)
+    for line in lines:
+        if "WARNING" in line:
+            warn.append(line)
 
 def find_ips():
     with open("sample.log", "r") as file:
         lines = file.readlines()
-        ips = set()
-        for line in lines:
-            found_ips = re.findall(r'\b(?:\d{1,3}\.){3}\d{1,3}\b', line)
-            for ip in found_ips:
-                ips.add(ip)
-        print(ips) 
+    ips = set()
+    for line in lines:
+        found_ips = re.findall(r'\b(?:\d{1,3}\.){3}\d{1,3}\b', line)
+        for ip in found_ips:
+            ips.add(ip)
 
-print(ips)
+    print(ips)
 
-with open("ips.txt", "w") as f:
-    for ip in ips:
-        f.write(ip + "\n")
+    with open("ips.txt", "w") as f:
+        for ip in ips:
+            f.write(ip + "\n")
+
+    return ips
 
 if __name__ == "__main__":
     ask = int(input("Choose options, print 1 or 2 or 3\n"
                     "1) Errors\n"
                     "2) Warnings\n"
-                    "3) Ips \n"))
+                    "3) IPs\n"))
 
     if ask == 1:
         read_log()
@@ -47,8 +47,8 @@ if __name__ == "__main__":
         count_warnings()
         print(warn)
     elif ask == 3:
-        find_ips()
+        ips = find_ips()
     else:
         print("Choose correct option")
 
-print(f"Errors: {len(errors)}, Warnings: {len(warn)}, IPs: {len(ips)}")
+    print(f"Errors: {len(errors)}, Warnings: {len(warn)}, IPs: {len(ips) if 'ips' in locals() else 0}")
