667d6eac01dc4423df6d44004211a4a471d2cf5a
VladVynnytskyi <vynnytskyivladyslav@gmail.com>
2025-08-03 21:04:14 +0200

    Update main.py

--- scr/main.py
+++ scr/main.py
@@ -1,3 +1,6 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from scr.reader import read_log
 from scr.warnings import count_warnings
 from scr.ip_extractor import find_ips
